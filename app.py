68672c616514a6033d38c6472443955b21823feb
Mansi Somaiya <58207628+somaiyamansi@users.noreply.github.com>
2020-07-26 05:03:10 +0530

    no changes in models.py

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.sqlite3"
 
 db = SQLAlchemy(app)
+admin = Admin(app)
 
 
 #exitfrom routes import *
