4c20519f34cb51a6bf87495b80e9697b378ab214
ayesharashid <ayesharashid011@users.noreply.github.com>
2020-07-25 18:53:48 +0530

    text-to-speech

---
+++ Feedback/text_to_speech/text_to_speech.py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+import os
+def text_to_speech(filename, fout, lngg):
+    fh = open(filename,"r")
+    mytext = fh.read().replace("\n", " ")
+
+    language = lngg
+
+    output = gTTS(text = mytext, lang=language, slow=False)
+
+    output.save(fout)
+    fh.close()
+
+if __name__ == "__main__":
+    textfile = "text.txt"
+    fout = "output.mp3"
+    lang = 'en'
+    text_to_speech(textfile, fout, lang)
+#to play the file from os perspective
+#os.system("start output.mp3")
