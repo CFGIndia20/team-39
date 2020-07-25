4bee2aec22c396fa8c861dee0895325b294fecbc
Aditya kumar Singh <42119124+ramrap62@users.noreply.github.com>
2020-07-25 19:39:00 +0530

    Update models.py

--- App/models.py
+++ App/models.py
@@ -1,1 +1,28 @@
+from app import db
 
+
+class Users(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    fname = db.Column('First Name',db.String(50))
+    lname = db.Column('Last Name',db.String(50))
+    username = db.Column('Username',db.String(50))
+    password = db.Column('Password',db.String(50))
+    contactno = db.Column('Contact No',db.Integer)
+    location = db.Column('Location',db.String(50))
+    role = db.Column('Role',db.String(20))
+
+
+class Units(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column('Unit Name',db.String(50))
+
+
+class Patients(db.Model):
+    id = db.Column('ID',db.Integer,primary_key= True)
+    uuid = db.Column('Patient User ID',db.String(50))
+    fname = db.Column('First Name',db.String(50))
+    lname = db.Column('Last Name',db.String(50))
+    username = db.Column('Username',db.String(50))
+    password = db.Column('Password',db.String(50))
+    contactno = db.Column('Contact No',db.Integer)
+    location = db.Column('Location',db.String(50))
