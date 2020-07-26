1f50ba0f8f1e339a5f3fcde597b33b9349d58098
Mansi Somaiya <58207628+somaiyamansi@users.noreply.github.com>
2020-07-26 08:38:11 +0530

    configure routes and templates

--- App/routes.py
+++ App/routes.py
@@ -1,9 +1,16 @@
 from App import db, app
 from flask import render_template,request, url_for
+from flask_login import login_user, current_user, logout_user, login_required
+import secrets
 
-@app.route('/admin')
+@app.route("/")
+@app.route("/home")
+def home():
+    render_template("home.html")
+
+@app.route('/sysadmin')
 def Admin():
-    return render_template('admin.html')
+    return render_template('admin_home.html')
 
 @app.route("/dashboard")
 def dashboard():
